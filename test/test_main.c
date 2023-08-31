c689608709e40dd2226cba841cfd88d64a22e0c5
Dementev Nikita <nikdem94@gmail.com>
2023-08-31 14:49:57 +0300

    dummy test

---
+++ test/test_main.c
@@ -0,0 +1,20 @@
+#include <unity.h>
+#include <stdio.h>
+#include <esp_system.h>
+
+void test_dummy(void)
+{
+  TEST_ASSERT_EQUAL(1, 1);
+}
+
+void app_main()
+{
+  UNITY_BEGIN();
+
+  RUN_TEST(test_dummy);
+  RUN_TEST(test_dummy);
+  RUN_TEST(test_dummy);
+
+  int res = UNITY_END();
+  esp_restart();
+}
